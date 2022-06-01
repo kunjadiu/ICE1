afed75b5c70346ee6d1e24a08ee1de126d097938
Utsav Kunjadiya <kunjadiu@sheridancollege.ca>
2022-06-01 12:37:53 -0400

    Update Card.java

--- src/cardtrickice1/Card.java
+++ src/cardtrickice1/Card.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ Student Id : 991657803
  */
 package cardtrickice1;
 
